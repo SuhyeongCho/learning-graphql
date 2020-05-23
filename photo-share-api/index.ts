c7d8abac01ee047087defef6dfd5d19c35017c58
SuhyeongCho <ch5951130@naver.com>
2020-05-23 18:32:45 +0900

    mongodb 설치

--- photo-share-api/index.ts
+++ photo-share-api/index.ts
@@ -6,28 +6,45 @@ const expressPlayground = require('graphql-playground-middleware-express')
 const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
 
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
 var typeDefs = readFileSync('./typeDefs.graphql', 'utf-8');
 const resolvers = require('./resolvers');
 
-/** 'express()' 를 호출하여 익스프레스 애플리케이션을 만듭니다. */
-const app = express();
+async function start() {
+  /** 'express()' 를 호출하여 익스프레스 애플리케이션을 만듭니다. */
+  const app = express();
 
-/**
- * 서버 인스턴스를 새로 만듭니다.
- * typeDefs(스키마)와 리졸버를 객체에 넣어 전달합니다.
- */
-const server = new ApolloServer({ typeDefs, resolvers });
+  const MONGO_DB = process.env.DB_HOST;
 
-/** 'applyMiddleWare()'를 호출하여 미들웨어가 같은 경로에 마운트되도록 합니다. */
-server.applyMiddleware({ app });
+  const client = await MongoClient.connect(MONGO_DB, { useNewUrlParser: true });
 
-/** 홈 라우트를 만듭니다. */
-app.get('/', (req, res) => res.end('PhotoShare API 에 오신 것을 환영합니다.'));
-app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+  const db = client.db();
 
-/** 특정 포트에서 리스닝을 시작합니다. */
-app.listen({ port: 4000 }, () =>
-  console.log(
-    `GraphQL Server running @ http://localhost:4000${server.graphqlPath}`
-  )
-);
+  const context = { db };
+
+  /**
+   * 서버 인스턴스를 새로 만듭니다.
+   * typeDefs(스키마)와 리졸버를 객체에 넣어 전달합니다.
+   */
+  const server = new ApolloServer({ typeDefs, resolvers, context });
+
+  /** 'applyMiddleWare()'를 호출하여 미들웨어가 같은 경로에 마운트되도록 합니다. */
+  server.applyMiddleware({ app });
+
+  /** 홈 라우트를 만듭니다. */
+  app.get('/', (req, res) =>
+    res.end('PhotoShare API 에 오신 것을 환영합니다.')
+  );
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+
+  /** 특정 포트에서 리스닝을 시작합니다. */
+  app.listen({ port: 4000 }, () =>
+    console.log(
+      `GraphQL Server running @ http://localhost:4000${server.graphqlPath}`
+    )
+  );
+}
+
+start();
